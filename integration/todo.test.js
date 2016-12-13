55c15921ae040c966b9e3a563cd10a0aff666ee3
Jeff Dean <jeff@galvanize.com>
2016-12-13 01:02:15 -0700

    refactor to chai-as-promised

--- integration/todo.test.js
+++ integration/todo.test.js
@@ -1,12 +1,15 @@
-const expect = require('chai').expect
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
 describe('mad libs', function() {
+  const h1 = element(by.css('h1'))
+
   it('should work for mad libs', function() {
     browser.get(`file://${__dirname}/index.html`);
 
-    element(by.css('h1')).getText().then((text) => {
-      expect(text).to.equal('Some text')
-    })
+    expect(h1.getText()).to.eventually.eq('Some text');
 
     browser.getTitle().then(function (title) {
       expect(title).to.eq('what')
