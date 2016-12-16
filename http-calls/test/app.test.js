c1b89a4e852d56b74fd2a69c711b796c9823d04f
Jeff Dean <jeff@galvanize.com>
2016-12-16 00:11:51 -0700

    prepare http-calls for students

--- http-calls/test/app.test.js
+++ http-calls/test/app.test.js
@@ -5,28 +5,28 @@ describe('App', function() {
   })
 
   it('can add expenses', function() {
-    element(by.model('$ctrl.expense.category')).sendKeys('Flyers')
-    element(by.model('$ctrl.expense.amount')).sendKeys('34.67')
+    element(by.css('#new-category')).sendKeys('Flyers')
+    element(by.css('#new-amount')).sendKeys('34.67')
     element(by.buttonText('Add Expense')).click()
 
     // check that the expense was added to the DOM
-    expect(element.all(by.repeater('expense in $ctrl.expenses')).count()).toEqual(1)
+    expect(element.all(by.css('tbody > tr')).count()).toEqual(1)
     // check that the expense was persisted
     browser.get(`/`)
-    expect(element.all(by.repeater('expense in $ctrl.expenses')).count()).toEqual(1)
+    expect(element.all(by.css('tbody > tr')).count()).toEqual(1)
 
-    element(by.model('$ctrl.expense.category')).sendKeys('Signs')
-    element(by.model('$ctrl.expense.amount')).sendKeys('4.45')
+    element(by.css('#new-category')).sendKeys('Signs')
+    element(by.css('#new-amount')).sendKeys('4.45')
     element(by.buttonText('Add Expense')).click()
-    expect(element.all(by.repeater('expense in $ctrl.expenses')).count()).toEqual(2)
+    expect(element.all(by.css('tbody > tr')).count()).toEqual(2)
 
     element(by.cssContainingText('tr', 'Flyers')).element(by.linkText('edit')).click()
-    expect(element(by.model('$ctrl.editingExpense.category')).getAttribute('value')).toEqual("Flyers");
-    expect(element(by.model('$ctrl.editingExpense.amount')).getAttribute('value')).toEqual("34.67");
+    expect(element(by.css('#edit-category')).getAttribute('value')).toEqual("Flyers");
+    expect(element(by.css('#edit-amount')).getAttribute('value')).toEqual("34.67");
 
-    element(by.model('$ctrl.editingExpense.category')).sendKeys('!!')
-    element(by.model('$ctrl.editingExpense.amount')).clear()
-    element(by.model('$ctrl.editingExpense.amount')).sendKeys('55.55')
+    element(by.css('#edit-category')).sendKeys('!!')
+    element(by.css('#edit-amount')).clear()
+    element(by.css('#edit-amount')).sendKeys('55.55')
     element(by.buttonText('Update Expense')).click()
 
     expect(element(by.cssContainingText('tr', 'Flyers!!')).getText()).toMatch(/Flyers!!/)
