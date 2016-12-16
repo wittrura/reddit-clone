abe22251a7a36d9b42fbc84090db68850e961c9f
Jeff Dean <jeff@galvanize.com>
2016-12-16 00:28:34 -0700

    simplify tests

--- list-management/test/conf.js
+++ list-management/test/conf.js
@@ -5,4 +5,18 @@ exports.config = {
   },
   directConnect: true,
   specs: ['*.test.js'],
+  onPrepare: function () {
+    const path = require('path')
+    const http = require('http')
+    const express = require('express')
+    const basePath = path.join(__dirname, '/../')
+
+    const app = express()
+      .use(express.static(basePath))
+      .get('*', (_, res) => res.sendFile( path.join(basePath, 'index.html') ))
+
+    const server = http.createServer(app)
+    server.listen(0)
+    browser.baseUrl = 'http://localhost:' + server.address().port
+  }
 }
