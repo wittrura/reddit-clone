17d76ccdb081213f0ea4f655d79d720549d75855
Jeff Dean <jeff@galvanize.com>
2016-12-18 00:41:14 -0700

    updated list management to use page model

--- list-management/test/app.test.js
+++ list-management/test/app.test.js
@@ -2,24 +2,64 @@ describe('App', function() {
   beforeEach(() => browser.get(`/`))
 
   it('loads with 2 items pre-populated', function() {
-    expect(element.all(by.repeater('item in $ctrl.items')).count()).toEqual(2)
+    new ListManagement()
+      .expectElements(2)
   })
 
   it('allows a user to add to the list', function() {
-    element(by.css('#name')).sendKeys('Gnome')
-    element(by.css('#quantity')).sendKeys('34')
-    element(by.buttonText('Add Item')).click()
-
-    expect(element.all(by.repeater('item in $ctrl.items')).get(2).getText()).toMatch(/Gnome 34/)
-
-    expect(element(by.css('#name')).getAttribute('value')).toEqual("")
-    expect(element(by.css('#quantity')).getAttribute('value')).toEqual("")
+    new ListManagement()
+      .addItem()
+      .expectItemToBeAdded()
+      .expectFormToBeCleared()
   })
 
   it('allows a user to delete from the list', function() {
-    element(by.cssContainingText('div', 'Couch 2')).element(by.linkText('Delete')).click()
-
-    expect(element.all(by.repeater('item in $ctrl.items')).count()).toEqual(1)
+    new ListManagement()
+      .deleteItem()
+      .expectItemDeleted()
   })
+
+  class ListManagement {
+
+    constructor() {
+      this.items = element.all(by.repeater('item in $ctrl.items'))
+      this.nameField = element(by.css('#name'))
+      this.quantityField = element(by.css('#quantity'))
+    }
+
+    addItem() {
+      this.nameField.sendKeys('Gnome')
+      this.quantityField.sendKeys('34')
+      element(by.buttonText('Add Item')).click()
+      return this
+    }
+
+    deleteItem() {
+      element(by.cssContainingText('div', 'Couch 2')).element(by.linkText('Delete')).click()
+      return this
+    }
+
+    expectElements(num) {
+      expect(this.items.count()).toEqual(num)
+      return this
+    }
+
+    expectItemToBeAdded() {
+      expect(this.items.get(2).getText()).toMatch(/Gnome 34/)
+      return this
+    }
+
+    expectFormToBeCleared() {
+      expect(this.nameField.getAttribute('value')).toEqual("")
+      expect(this.quantityField.getAttribute('value')).toEqual("")
+      return this
+    }
+
+    expectItemDeleted() {
+      expect(this.items.count()).toEqual(1)
+      return this
+    }
+
+  }
 
 })
