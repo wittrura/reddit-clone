cdea75931281d1c575f526c59f61b317ef476f57
Jeff Dean <jeff@galvanize.com>
2016-12-18 01:02:23 -0700

    updated simple-interaction tests to page model

--- simple-interaction/test/app.test.js
+++ simple-interaction/test/app.test.js
@@ -3,20 +3,40 @@ describe('App', function() {
   beforeEach(() => browser.get(`/`))
 
   it('formats the nickname correctly', function() {
-    element(by.model('$ctrl.firstName')).sendKeys('Dwayne')
-    element(by.model('$ctrl.nickname')).sendKeys('the Rock')
-    element(by.model('$ctrl.lastName')).sendKeys('Johnson')
-    element(by.buttonText('Submit')).click()
-
-    expect(element(by.css('p:last-child')).getText()).toEqual('Dwayne "the Rock" Johnson')
+    new AppTest().expectFormatWithNickname()
   })
 
   it('formats the name correctly without a nickname', function() {
-    element(by.model('$ctrl.firstName')).sendKeys('Dwayne')
-    element(by.model('$ctrl.lastName')).sendKeys('Johnson')
-    element(by.buttonText('Submit')).click()
-
-    expect(element(by.css('p:last-child')).getText()).toEqual('Dwayne Johnson')
+    new AppTest().expectFormatWithoutNickname()
   })
+
+  class AppTest {
+
+    constructor() {
+      this.firstNameField = element(by.model('$ctrl.firstName'))
+      this.nicknameField = element(by.model('$ctrl.nickname'))
+      this.lastNameField = element(by.model('$ctrl.lastName'))
+      this.submitButton = element(by.buttonText('Submit'))
+      this.lastItem = element(by.css('p:last-child'))
+    }
+
+    expectFormatWithNickname() {
+      this.firstNameField.sendKeys('Dwayne')
+      this.nicknameField.sendKeys('the Rock')
+      this.lastNameField.sendKeys('Johnson')
+      this.submitButton.click()
+
+      expect(this.lastItem.getText()).toEqual('Dwayne "the Rock" Johnson')
+    }
+
+    expectFormatWithoutNickname() {
+      this.firstNameField.sendKeys('Dwayne')
+      this.lastNameField.sendKeys('Johnson')
+      this.submitButton.click()
+
+      expect(this.lastItem.getText()).toEqual('Dwayne Johnson')
+    }
+
+  }
 
 })
