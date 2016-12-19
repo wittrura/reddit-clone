bb4990a968bb7f52c19255e2d265df62b5f2a1bb
Jeff Dean <jeff@galvanize.com>
2016-12-19 00:00:51 -0700

    added script to run all tests

---
+++ all.sh
@@ -0,0 +1,12 @@
+cd simple-interaction && npm test
+cd ../
+cd list-management && npm test
+cd ../
+cd http-calls && npm test
+cd ../
+cd ui-router-fundamentals && npm test
+cd ../
+cd reddit-clone-part-1 && npm test
+cd ../
+cd reddit-clone-part-2 && npm test
+cd ../
