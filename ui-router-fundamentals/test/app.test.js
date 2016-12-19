a0bbd954e572876972cedf3bfded8cce57981d08
Jeff Dean <jeff@galvanize.com>
2016-12-18 22:29:01 -0700

    moved ui-router-fundamentals to page model

--- ui-router-fundamentals/test/app.test.js
+++ ui-router-fundamentals/test/app.test.js
@@ -3,21 +3,62 @@ describe('App', function() {
   beforeEach(() => browser.get(`/`))
 
   it('can manage houses', function() {
-    expect(element.all(by.repeater('house in $ctrl.houses')).count()).toEqual(1)
-
-    element(by.linkText('Add House')).click()
-    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/houses/new");
-
-    element(by.model('$ctrl.house.name')).sendKeys('Room with a view')
-    element(by.model('$ctrl.house.address')).sendKeys('10-25 Main St')
-    element(by.buttonText('Create House')).click()
-    expect(browser.getCurrentUrl()).toMatch("/houses/\\d");
-
-    element(by.linkText('Return Home')).click()
-    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/");
-
-    element(by.linkText('Room with a view')).click()
-    expect(browser.getCurrentUrl()).toMatch("/houses/\\d");
+    new HouseTest()
+      .clickOnAddHouseLink()
+      .fillInAndSubmitForm()
+      .expectToBeOnTheShowPage()
+      .goToHomePage()
+      .expectNewHouseToBeThere()
+      .clickOnHouseLink()
+      .expectToBeOnTheShowPage()
   })
+
+  class HouseTest {
+
+    constructor() {
+      this.houses = element.all(by.repeater('house in $ctrl.houses'))
+      this.addHouseLink = element(by.linkText('Add House'))
+      this.nameField = element(by.model('$ctrl.house.name'))
+      this.addressField = element(by.model('$ctrl.house.address'))
+      this.createHouseButton = element(by.buttonText('Create House'))
+      this.returnHomeLink = element(by.linkText('Return Home'))
+    }
+
+    clickOnAddHouseLink() {
+      expect(this.houses.count()).toEqual(1)
+      this.addHouseLink.click()
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/houses/new")
+      return this
+    }
+
+    fillInAndSubmitForm() {
+      this.nameField.sendKeys('Room with a view')
+      this.addressField.sendKeys('10-25 Main St')
+      this.createHouseButton.click()
+      return this
+    }
+
+    goToHomePage() {
+      this.returnHomeLink.click()
+      return this
+    }
+
+    expectNewHouseToBeThere() {
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/");
+      expect(this.houses.count()).toEqual(2)
+      return this
+    }
+
+    clickOnHouseLink() {
+      element(by.linkText('Room with a view')).click()
+      return this
+    }
+
+    expectToBeOnTheShowPage() {
+      expect(browser.getCurrentUrl()).toMatch("/houses/\\d");
+      return this
+    }
+
+  }
 
 })
