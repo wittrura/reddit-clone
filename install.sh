bda8415c00f3778b0a569cd34bbd5e2c0b662d0d
Michael Herman <hermanmu@gmail.com>
2017-01-16 19:46:13 -0700

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd simple-interaction && npm install
+cd ../
+cd list-management && npm install
+cd ../
+cd http-calls && npm install
+cd ../
+cd ui-router-fundamentals && npm install
+cd ../
+cd reddit-clone-part-1 && npm install
+cd ../
+cd reddit-clone-part-2 && npm install
+cd ../
