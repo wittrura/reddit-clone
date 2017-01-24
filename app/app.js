2606b856c9ee7510ba564188b8e351f2c5d2cda0
CJ R <hello@cjr.co.de>
2017-01-24 13:31:55 -0700

    :bug: Serve static node_modules from app

--- app/app.js
+++ app/app.js
@@ -4,7 +4,12 @@ const express = require('express')
 const basePath = path.join(__dirname, '/../')
 
 function app(basePath) {
-	return express().use(express.static(basePath)).get('*', (_, res) => res.sendFile(path.join(basePath, 'index.html')));
+	const app = express();
+	app.use(express.static(basePath));
+	app.use('/node_modules', express.static(path.join(basePath, 'node_modules')));
+	app.get('*', (_, res) => res.sendFile(path.join(basePath, 'index.html')));
+
+	return app;
 }
 
 module.exports = app;
