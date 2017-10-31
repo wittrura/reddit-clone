03fd7d056a938363c9672ad8dea4641d0462bc98
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-31 15:29:23 -0500

    refactor: moves javascript to separate file, updates to html template

--- simple-interaction/app.js
+++ simple-interaction/app.js
@@ -1,4 +1,18 @@
 (function() {
   'use strict'
+  angular.module('app', [])
+    .component('app', {
+      controller: function () {
+        const vm = this
 
+        vm.updateFullName = function () {
+          if (vm.nickname) {
+            vm.fullName = `${vm.firstName} "${vm.nickname}" ${vm.lastName}`;
+          } else {
+            vm.fullName = `${vm.firstName} ${vm.lastName}`;
+          }
+        }
+      },
+      templateUrl: 'namesForm.html'
+    });
 }());
