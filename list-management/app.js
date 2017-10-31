b2ab8a20c1f57872d858dc2d7bfb017b0f0ea0ad
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-31 16:19:23 -0500

    feat: updates app to display list of items with ability to add and delete

--- list-management/app.js
+++ list-management/app.js
@@ -2,6 +2,27 @@
   'use strict'
 
   angular.module('app', [])
-    .component('cart', {})
+    .component('cart', {
+      controller: function() {
+        const vm = this
 
+        vm.$onInit = function () {
+          vm.items = [
+            {name: 'Apples', quantity: 4},
+            {name: 'Avocados', quantity: 2}
+          ];
+        }
+
+        vm.createItem = function() {
+          vm.items.push(vm.item);
+          delete vm.item;
+        }
+
+        vm.deleteItem = function(e, item) { // include event for preventDefault
+          e.preventDefault();
+          vm.items.splice(vm.items.indexOf(item), 1);
+        }
+      },
+      templateUrl: 'cartForm.html'
+    })
 }());
