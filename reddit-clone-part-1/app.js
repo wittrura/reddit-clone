ffa8b97d17e42a26f5a67cd77c382dbc89ad4087
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-02 15:30:48 -0500

    feat: adds ability to upvote and downvote posts

--- reddit-clone-part-1/app.js
+++ reddit-clone-part-1/app.js
@@ -6,7 +6,11 @@
 
       vm.$onInit = function () {
         vm.newPostFormDisplay = true;
-        vm.posts = [];
+        vm.posts = [
+          {title: 'this is a guy with a beard', body: 'what is with all the beards?', author: 'bondy', imageUrl: 'https://static.pexels.com/photos/211050/pexels-photo-211050.jpeg', score: 0},
+          {title: 'more beards', body: 'srsly?', author: 'russ', imageUrl: 'https://static.pexels.com/photos/69212/pexels-photo-69212.jpeg', score: 0},
+          {title: 'not trying at all', body: "she's so... uniqiue", author: 'ryan', imageUrl: 'https://static.pexels.com/photos/192440/pexels-photo-192440.jpeg', score: 0}
+        ];
       }
 
       vm.toggleNewPostForm = function() {
@@ -23,6 +27,14 @@
       vm.deletePost = function (e, post) {
         e.preventDefault();
         vm.posts.splice(vm.posts.indexOf(post), 1);
+      }
+
+      vm.upvotePost = function (post) {
+        post.score += 1;
+      }
+
+      vm.downvotePost = function (post) {
+        if (post.score > 0) post.score -= 1;
       }
     },
 
