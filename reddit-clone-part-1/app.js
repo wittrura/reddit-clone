5527597506478bdf1cea618ad40390ca5c8568fc
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-02 10:50:11 -0500

    feat: binds form elements to controller, adds posts view

--- reddit-clone-part-1/app.js
+++ reddit-clone-part-1/app.js
@@ -6,10 +6,22 @@
 
       vm.$onInit = function () {
         vm.newPostFormDisplay = true;
+        vm.posts = [];
       }
 
       vm.toggleNewPostForm = function() {
         vm.newPostFormDisplay = !vm.newPostFormDisplay;
+      }
+
+      vm.createPost = function (e) {
+        e.preventDefault();
+        vm.posts.push(vm.newPost);
+        delete vm.newPost;
+      }
+
+      vm.deletePost = function (e, post) {
+        e.preventDefault();
+        vm.posts.splice(vm.posts.indexOf(post), 1);
       }
     },
 
