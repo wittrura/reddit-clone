2868fb403bccecbf5ab64cd16f3e0afcc58f153f
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-03 14:50:24 -0500

    feat: adds ability to display and hide comments

--- reddit-clone-part-1/app.js
+++ reddit-clone-part-1/app.js
@@ -5,13 +5,36 @@
       const vm = this;
 
       vm.$onInit = function () {
-        vm.newPostFormDisplay = true;
+        vm.newPostFormDisplay = false;
         vm.propertyName = 'score';
         vm.reverse = true;
         vm.posts = [
-          {title: 'this is a guy with a beard', body: 'what is with all the beards?', author: 'bondy', imageUrl: 'https://static.pexels.com/photos/211050/pexels-photo-211050.jpeg', score: 0, date: new Date(2017, 10, 1, 9, 10, 20, 30)},
-          {title: 'more beards', body: 'srsly?', author: 'russ', imageUrl: 'https://static.pexels.com/photos/69212/pexels-photo-69212.jpeg', score: 0, date: new Date(2017, 10, 2, 9, 20, 20, 30)},
-          {title: 'not trying at all', body: "she's so... uniqiue", author: 'ryan', imageUrl: 'https://static.pexels.com/photos/192440/pexels-photo-192440.jpeg', score: 0, date: new Date(2017, 10, 2, 16, 23, 40, 30)}
+          {
+            title: 'this is a guy with a beard',
+            body: 'what is with all the beards?',
+            author: 'bondy',
+            imageUrl: 'https://static.pexels.com/photos/211050/pexels-photo-211050.jpeg',
+            score: 0,
+            date: new Date(2017, 10, 1, 9, 10, 20, 30),
+            comments: [{body: 'Firsties!'}, {body: 'I did it for the lulz'}]
+          },
+          {
+            title: 'more beards',
+            body: 'srsly?',
+            author: 'russ',
+            imageUrl: 'https://static.pexels.com/photos/69212/pexels-photo-69212.jpeg',
+            score: 0,
+            date: new Date(2017, 10, 2, 9, 20, 20, 30),
+            comments: [{body: 'so fly!'}, {body: 'what a goon'}]
+          },{
+            title: 'not trying at all',
+            body: "she's so... uniqiue",
+            author: 'ryan',
+            imageUrl: 'https://static.pexels.com/photos/192440/pexels-photo-192440.jpeg',
+            score: 0,
+            date: new Date(2017, 10, 2, 16, 23, 40, 30),
+            comments: [{body: 'i bet she liked stuff before it was cool'}]
+          }
         ];
       }
 
@@ -23,6 +46,7 @@
         e.preventDefault();
         vm.newPost.score = 0;
         vm.newPost.date = new Date();
+        vm.newPost.showComments = false;
         vm.posts.push(vm.newPost);
         delete vm.newPost;
         vm.newPostForm.$setPristine();
