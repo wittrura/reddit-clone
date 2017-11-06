f9ab3c109e809f6519200d025b84d75399bd36ae
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-06 12:51:53 -0600

    feat: adds ability to edit and delete expenses

--- http-calls/app/public/js/expenses.component.js
+++ http-calls/app/public/js/expenses.component.js
@@ -19,34 +19,46 @@
 
     vm.addExpense = function() {
       $http.post('/api/expenses', vm.newExpense).then(function (response) {
+        // update viewmodel without needing to make another api call
         vm.expenses.push(response.data);
       });
     }
 
     vm.toggleEditForm = function(expense) {
-      // clear form
-      delete vm.editedExpense;
+      delete vm.editedExpense; // clear form
 
       // if the form has only been clicked once, display the form and update expenseToEdit
       // if a different edit link is clicked, display the form and update expenseToEdit
       if (!vm.expenseToEdit || vm.expenseToEdit.id !== expense.id) {
         vm.showEditForm = true;
-        // for storing expense that will be updated
-        vm.expenseToEdit = expense;
+        vm.expenseToEdit = expense; // for storing expense that will be updated
       } else {
         vm.showEditForm = !vm.showEditForm;
       }
-
-      console.log(vm.expenseToEdit);
     }
 
     vm.updateExpense = function(expense) {
-      console.log(expense);
-      console.log(vm.editedExpense);
+      $http.patch(`/api/expenses/${expense.id}`, vm.editedExpense).then(function (response) {
+        // find the index of the updated expense
+        for (var i = 0; i < vm.expenses.length; i++) {
+          if (vm.expenses[i].id === expense.id) {
+            // update values on viewmodel
+            vm.expenses[i] = response.data;
+          }
+        }
+      });
     }
 
     vm.destroy = function(expense) {
-      console.log(expense);
+      $http.delete(`/api/expenses/${expense.id}`).then(function (response) {
+        // find the index of the updated expense
+        for (var i = 0; i < vm.expenses.length; i++) {
+          if (vm.expenses[i].id === expense.id) {
+            // update values on viewmodel
+            vm.expenses.splice(i, 1);
+          }
+        }
+      });
     }
   }
 
