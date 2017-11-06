5d3f18b9030c2abf498f9f2c2b8af0726ff249bb
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-06 11:40:42 -0600

    feat: adds ability to retrieve and create expenses" "

--- http-calls/app/public/js/expenses.component.js
+++ http-calls/app/public/js/expenses.component.js
@@ -4,10 +4,24 @@
   angular.module('app')
     .component('expenses', {
       controller: controller,
-      template: ``
+      templateUrl: 'js/expenses.component.html'
     })
 
-  function controller() {
+  controller.$inject = ['$http'];
+  function controller($http) {
+    const vm = this;
+
+    vm.$onInit = function () {
+      $http.get('/api/expenses').then(function (response) {
+        vm.expenses = response.data;
+      });
+    }
+
+    vm.addExpense = function() {
+      $http.post('/api/expenses', vm.newExpense).then(function (response) {
+        vm.expenses.push(response.data);
+      });
+    }
   }
 
 }());
