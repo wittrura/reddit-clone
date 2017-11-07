68ee481826cbe7bf50670ec89db7355e69beb858
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-07 13:58:58 -0600

    config: sets up routes for home page and new house

--- ui-router-fundamentals/js/app.config.js
+++ ui-router-fundamentals/js/app.config.js
@@ -4,9 +4,25 @@
   angular.module('app').config(config)
 
   // TODO: figure out how to configure the app correctly
+  // config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+  function config($stateProvider, $urlRouterProvider, $locationProvider) {
 
-  function config() {
-    
+    $locationProvider.html5Mode({
+      enabled: true
+    });
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        component: 'houseList'
+      })
+      .state('new', {
+        url: '/houses/new',
+        component: 'houseNew'
+      })
+
   }
 
 }());
