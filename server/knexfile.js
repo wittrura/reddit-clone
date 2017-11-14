f8e5b234ce1c4b1bffbc00c9d5b3d5f2a0d96ce5
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-14 13:44:00 -0600

    bug: adds production property for knex

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,6 +12,11 @@ module.exports = {
     connection: {
       database: process.env.DATABASE_URL || 'reddit-clone-test',
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
   }
 
 };
